83d2ac875b576b659f795d16d9e07d3c0805f526
sedyounes <sedyounesmousavi1388@gmail.com>
2026-08-24 15:38:05 +0330

    debug

--- app_portfolio/serializers.py
+++ app_portfolio/serializers.py
@@ -7,20 +7,38 @@ class PortfolioSerializer(serializers.ModelSerializer):
         model = Portfolio
         fields = '__all__'
 
+
 class AddPortfolioSerializer(serializers.ModelSerializer):
     name = serializers.CharField()
     broker = serializers.CharField()
-    balance = serializers.IntegerField()
-    currency = serializers.ChoiceField(choices=[
-        'USD',
-        'USDT',
-        'EUR',
-        'IRR'
-    ])
-    leverage = serializers.ChoiceField(choices=[
-        '1:1',
-        '1:30',
-        '1:100',
-        '1:200',
-        '1:500'
-    ])
+    balance = serializers.DecimalField(
+        max_digits=15,
+        decimal_places=2
+    )
+    currency = serializers.ChoiceField(
+        choices=[
+            'USD',
+            'USDT',
+            'EUR',
+            'IRR'
+        ]
+    )
+    leverage = serializers.ChoiceField(
+        choices=[
+            '1:1',
+            '1:30',
+            '1:100',
+            '1:200',
+            '1:500'
+        ]
+    )
+
+    class Meta:
+        model = Portfolio
+        fields = [
+            'name',
+            'broker',
+            'balance',
+            'currency',
+            'leverage',
+        ]
