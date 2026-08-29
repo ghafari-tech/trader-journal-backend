0a84094986b9c7754a78b9fcaebe5346fbcd0405
Younes <sedyounesmousavi1388@gmail.com>
2026-08-29 20:25:32 +0330

    debug again trades list API

--- app_transaction/views.py
+++ app_transaction/views.py
@@ -1,5 +1,5 @@
 from django.db.models import Sum
-from drf_spectacular.utils import extend_schema
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
