651a2bc24a93326da09db701f02c6a87d34874b4
sedyounes <sedyounesmousavi1388@gmail.com>
2026-09-13 06:38:09 +0330

    add mt-status to urls

--- app_setting/urls.py
+++ app_setting/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('user-info/', views.user_info, name='user_info'),
     path('plan/', views.user_plan_info, name='user_plan_info'),
     path('metatrader/connect/', views.metatrader_connect, name='metatrader_connect'),
+    path('metatrader/mt-status/', views.metatrader_status, name='metatrader_status'),
     path('metatrader/heartbeat/', views.metatrader_heartbeat, name='metatrader_heartbeat'),
     path('metatrader/sync-transactions/', views.metatrader_sync_transactions, name='metatrader_sync_transactions'),
     path('metatrader/download-ea/', views.download_ea, name='download_ea'),
